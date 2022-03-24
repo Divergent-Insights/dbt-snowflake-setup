a83cf0f62c989f327609a6ead8e5c74c67083b00
Wilson Urdaneta <wilson.urdaneta@divergentinsights.com.au>
2022-03-24 10:08:56 +0800

    Adding generate schema macro

---
+++ data/macros/utils/generate_schema_name.sql
@@ -0,0 +1,19 @@
+{#
+    This macro overwrites dbt default generate_schema_name macro
+    More details: https://docs.getdbt.com/docs/building-a-dbt-project/building-models/using-custom-schemas
+#}
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{custom_schema_name | trim }}
+
+    {%- enfif -%}
+
+{%- endmacro %}
