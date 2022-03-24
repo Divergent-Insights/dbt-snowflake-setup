a2bba34c0ce56a1e351a1aea664a00899df0c4c7
Wilson Urdaneta <wilson.urdaneta@divergentinsights.com.au>
2022-03-24 09:53:33 +0800

    Fixing set metadata columns

--- data/macros/utils/set_metadata_columns.sql
+++ data/macros/utils/set_metadata_columns.sql
@@ -4,12 +4,12 @@
         Divergent Insights. All Rights Reserved
     #}
 
-    MD_DATA_SOURCE VARCHAR COMMENT 'Metadata columns';
-    MD_PIPELINE_ID VARCHAR COMMENT 'Metadata columns';
-    MD_BATCH_ID VARCHAR COMMENT 'Metadata columns';
-    MD_CREATION_DATE_UTC TIMESTAMP_TZ COMMENT 'Metadata columns';
-    MD_UPDATE_DATE_UTC TIMESTAMP_TZ COMMENT 'Metadata columns';
-    MD_PIPELINE_NAME VARCHAR COMMENT 'Metadata columns';
-    MD_COMMIT_ID VARCHAR COMMENT 'Metadata columns';
+    MD_DATA_SOURCE VARCHAR COMMENT 'Metadata columns',
+    MD_PIPELINE_ID VARCHAR COMMENT 'Metadata columns',
+    MD_BATCH_ID VARCHAR COMMENT 'Metadata columns',
+    MD_CREATION_DATE_UTC TIMESTAMP_TZ COMMENT 'Metadata columns',
+    MD_UPDATE_DATE_UTC TIMESTAMP_TZ COMMENT 'Metadata columns',
+    MD_PIPELINE_NAME VARCHAR COMMENT 'Metadata columns',
+    MD_COMMIT_ID VARCHAR COMMENT 'Metadata columns'
 
 { % endmacro %}
