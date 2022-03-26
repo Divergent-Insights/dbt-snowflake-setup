919dc5d9db378e94e1566bec2b805d7b1aecf175
Wilson Urdaneta <wilson.urdaneta@divergentinsights.com.au>
2022-03-27 05:51:42 +0800

    Adding get metadata columns macro

---
+++ data/macros/utils/get_metadata_columns.sql
@@ -0,0 +1,10 @@
+{% macro set_metadata_columns() %}
+
+    md_data_source,
+    md_pipeline_id,
+    md_batch_id,
+    md_creation_date_utc,
+    md_update_date_utc,
+    md_commit_id
+
+{% endmacro %}
